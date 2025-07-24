03ad5381cc7fc102358d56d0a5af43789783570a
tubakhan0415 <tubashadab7@gmail.com>
2025-07-24 12:56:12 +0530

    photo-backend

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-// app.js ✅ GOOD VERSION
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const sequelize = require("./config/db");
-const path = require("path");
 
+require("./models/Photo");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
-// Routes
 app.use("/uploads", express.static("uploads"));
 
-app.get("/", (req, res) => res.send("Construction SQL API is live"));
+// Routes
+app.use("/api/photos", require("./routes/photoRoutes"));
 
-// ✅ Sync DB
+app.get("/", (req, res) => {
+  res.send("Project Photos API is running...");
+});
+
+// Sync DB
 sequelize.sync({ alter: true })
   .then(() => console.log("✅ Database synced"))
   .catch((err) => console.error("❌ Sync error:", err));
 
-module.exports = app;  // DO NOT call app.listen() here!
+module.exports = app;
